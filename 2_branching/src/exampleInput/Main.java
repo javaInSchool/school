f125d21ecf32e8cfd49bf36d4ee602b215c3bef8
OOO <lejchack@mail.ru>
2018-10-05 12:26:29 +0300

    3_loops added

---
+++ 2_branching/src/exampleInput/Main.java
@@ -0,0 +1,13 @@
+package exampleInput;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите значение х: ");
+        int x = s.nextInt();
+
+        System.out.println(x);
+    }
+}
---
+++ 3_loops/src/example1/Main.java
@@ -0,0 +1,31 @@
+package example1;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println((i < 5) + " " + i);
+        }
+
+        for (int i = 5; i > 0; i--){
+            System.out.println((i > 0) + " " + i);
+        }
+
+        int i = 0;
+        for (int i = 0; i < 5; i++){
+            System.out.println((i < 5) + " " + i);
+        }
+
+        int i = 0;
+        for (i = 3; i < 5; i++){
+            System.out.println((i < 5) + " " + i);
+        }
+
+        for (int k = 0, a = 2; a + k < 5; k++, a= a+1){
+            System.out.println((k < 5) + " " + k);
+        }
+
+        for (int k = 0, int a = 2; a + k < 5; k++, a= a+1){
+            System.out.println((k < 5) + " " + k);
+        }
+    }
+}
