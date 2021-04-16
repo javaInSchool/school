671013e6e9facbfe6248299a448fe522042602ed
javaInSchool <gorbch.oleg87@gmail.com>
2021-04-16 13:14:18 +0300

    java9 update

--- src/java9/oop/Main.java
+++ src/java9/oop/Main.java
@@ -1,7 +1,13 @@
 package java9.oop;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File f = new File("D:/myFile.txt");
+        f.createNewFile();
+
         Swallow s = new Swallow();
         double dW = s.eat(4,"Fly");
         System.out.println(dW);
---
+++ src/java9/oop/MyApp.java
@@ -0,0 +1,14 @@
+package java9.oop;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage){
+        stage.show();
+    }
+}
