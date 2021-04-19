6b6bc40f2cfea00e3ab4acc7fd72d67bfc0762ba
javaInSchool <gorbch.oleg87@gmail.com>
2021-04-19 13:15:14 +0300

    les125 update

--- src/_11_javaFX/lesson125/Controller.java
+++ src/_11_javaFX/lesson125/Controller.java
@@ -1,5 +1,45 @@
 package _11_javaFX.lesson125;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+
+public class Controller {
+    public TextField w;
+    public TextField l;
+    public TextField d;
+    public Label result;
+    public Button add;
+    public Button add2;
+    @FXML
+    void initialize(){
+        add.setOnAction(ActionEvent -> {
+            System.out.println("Hello button");
+        });
+        add2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println(actionEvent.getEventType());
+            }
+        });
+    }
+
+    @FXML
+    public void calc(){
+        double wid = 0, len = 0, dep = 0;
+
+        try {
+            wid = Double.parseDouble(w.getText());
+            len = Double.parseDouble(l.getText());
+            dep = Double.parseDouble(d.getText());
+        }catch(NumberFormatException nfe){
+            result.setText("Невірне значення поля ");
+        }
+        double r = wid * len * dep;
+        result.setText("Об'єм акваріуму: " + r + " м3");
+    }
 }
