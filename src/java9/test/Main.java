34279a715b4927d430e1edf5fc282f2a897ed8aa
javaInSchool <gorbch.oleg87@gmail.com>
2021-04-23 09:48:58 +0300

    java9 test added

---
+++ src/java9/test/Main.java
@@ -0,0 +1,25 @@
+package java9.test;
+
+import java.math.BigInteger;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger n = null; // byte - 8, short - 16, int - 32, long - 64
+        while(true){
+            System.out.println("Введите целое число: ");
+            String text = in.nextLine();
+            try {
+                n = new BigInteger( text );
+                break;
+            }catch(InputMismatchException | NumberFormatException e){
+                System.out.println("Не верный ввод, повторите");
+            }finally {
+                System.out.println("ввод закончен...");
+            }
+        }
+        System.out.println("Вы ввели : " + n);
+    }
+}
